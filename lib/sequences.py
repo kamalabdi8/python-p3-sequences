900309552fb849daf237f3d4b5872f13827c28c4
kamalabdi8 <kamalabdi042@gmail.com>
2024-11-27 20:23:30 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])  # Print an empty list for length = 0
+        return
+    elif length == 1:
+        print([0])  # Print a list with one element for length = 1
+        return
+    
+    # Generate Fibonacci sequence for length >= 2
+    fibonacci = [0, 1]
+    for f_ in range(2, length):
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci)
